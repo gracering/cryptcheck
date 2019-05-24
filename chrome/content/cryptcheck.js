c29d507f45b701e98f772c517ae01949c10bd427
gracering <38143299+gracering@users.noreply.github.com>
2019-05-24 11:14:54 -0400

    Add files via upload

--- chrome/content/cryptcheck.js
+++ chrome/content/cryptcheck.js
@@ -1,1 +1,12 @@
-//base
+/*
+* TODO
+* 1. messge detection -> scan for incoming message/hang onto alert
+* 2. messge parsing -> parse new message to see if it's encrypted
+* 3. check encryption against protocol to determine if auto-reply should be sent
+* 5. record result -> where should this be stored?
+* 4. compose new message with appropriate information 
+* 5. send message to sender with "you messsed up" message
+* 6. alert end user that a message was sent
+*/
+
+//message detection
